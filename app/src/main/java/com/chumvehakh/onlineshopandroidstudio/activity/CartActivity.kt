9a1ff39477396f050b0a0a2fb8426f1f42ab4e98
Chum_Veha <chumveua02@gmail.com>
2024-07-11 20:24:39 +0700

    Final Commit

---
+++ app/src/main/java/com/chumvehakh/onlineshopandroidstudio/activity/CartActivity.kt
@@ -0,0 +1,17 @@
+package com.chumvehakh.onlineshopandroidstudio.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.chumvehakh.onlineshopandroidstudio.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_cart)
+
+    }
+}
